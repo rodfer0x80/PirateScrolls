f76d250a55b996eff496a51b9aa8c04490657f71
rodrigolf080 <rodrigolf080@gmail.com>
2021-03-21 17:04:09 +0000

    express server running; express mysql connection; handling GET and POST requests

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const express = require("express");
+const app = express();
 const mysql = require("mysql");
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+// Database
 // Create a connection object with the user details
 const  con = mysql.createPool({
   connectionLimit: 1,
@@ -9,17 +13,32 @@ const  con = mysql.createPool({
   database: "dunims",
   debug: false
 });
-
+let res = "USERS: ";
 // Query database
 let sql ="SELECT * FROM users";
-
-con.query(sql, (err, result) => {
+  con.query(sql, (err, result) => {
   if (err) {
     console.error("Error executing query: " + JSON.stringify(err));
   } else {
-      console.log(JSON.stringify(result));
+    //res += "QUERYED SUCCESSFULLY";
+  res += JSON.stringify(result);
   }
-});
+ });
 
-// Close connection when finished
-con.end();
+// Return Users table when '/' page is visited
+function returnUsers(request, response){
+  response.send(res);
+}
+
+// Handle GET requests
+// Root path
+app.get("/", returnUsers);
+
+// Handle POST requests
+// Root path
+app.post("/", (request, response) =>{
+  let req = request.body;
+  response.send("Received!\n");
+});
+// Listen on port <port_number>
+app.listen(8080);
